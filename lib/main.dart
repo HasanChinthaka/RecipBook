fe344d1658fac790aa3c22744a9af26fb898907b
Hasan Chinthaka <pgrhasanchinthaka@gmail.com>
2024-11-19 15:44:32 +0530

    Create Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:recepies_app/pages/home_page.dart';
 import 'package:recepies_app/pages/login_page.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   runApp(MyApp());
 }
 
 // ignore: use_key_in_widget_constructors
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    await Future.delayed(const Duration(seconds: 1));
+    FlutterNativeSplash.remove();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,10 +47,10 @@ class MyApp extends StatelessWidget {
         ),
       ),
       initialRoute: "/login",
-    routes: {
-      "/login" : (contex) => LoginPage(),
-      "/home" : (context) => HomePage(),
-    },
+      routes: {
+        "/login": (contex) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
     );
   }
 }
